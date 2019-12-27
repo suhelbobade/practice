0d2aac8100836ac214095017c526bf9364f59b15
suhelbobade <suhelbobade.sb@gmail.com>
2019-12-27 22:26:38 +0530

    FT;

---
+++ practice/src/main/java/oort/com/practice/Practice.java
@@ -0,0 +1,19 @@
+package oort.com.practice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Practice {
+    @RequestMapping("/Wellcome")
+    public List<String> Hey()
+    {
+        List<String> list = new ArrayList<>() ;
+        list.add("Suhel");
+        list.add("Sam");
+        return list;
+    }
+}
